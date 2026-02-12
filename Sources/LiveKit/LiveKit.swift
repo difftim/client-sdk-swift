b63259520fd6338a984fea019fb44a1db7c84922
luke <brainwith2019@gmail.com>
2026-02-12 21:44:11 +0800

    Fix audio player node life cycle

--- Sources/LiveKit/LiveKit.swift
+++ Sources/LiveKit/LiveKit.swift
@@ -33,7 +33,7 @@ public class LiveKitSDK: NSObject, Loggable {
     override private init() {}
 
     @objc(sdkVersion)
-    public static let version = "2.10.2-a7"
+    public static let version = "2.10.2-a8"
     static let ffiVersion = buildVersion()
 
     fileprivate struct State {
