5b2b9c25b56ae67e160c15bf05dcb826b7cdf886
luke <brainwith2019@gmail.com>
2026-06-16 20:35:38 +0800

    bump to 2.12.1-a23

--- Sources/LiveKit/LiveKit.swift
+++ Sources/LiveKit/LiveKit.swift
@@ -33,7 +33,7 @@ public class LiveKitSDK: NSObject, Loggable {
     override private init() {}
 
     @objc(sdkVersion)
-    public static let version = "2.12.1-a22"
+    public static let version = "2.12.1-a23"
     static let ffiVersion = buildVersion()
 
     fileprivate struct State {
